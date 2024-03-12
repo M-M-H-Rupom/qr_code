8f83f9f36c7add29e0c573a7fe6eb290e98b464c
rupom <mohrajulrupom@gmail.com>
2024-03-12 22:09:57 +0600

    qr code plugin using qr code api

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 function pqrc_display_qr_code($content){
     $get_current_id = get_the_ID();
     $current_post_title = get_the_title($get_current_id);
+    $current_post_permalink = get_the_permalink($get_current_id);
+    $get_post_type = get_post_type($get_current_id);
+    if('page'== $get_post_type){
+        return $content;
+    }
+    $image_src = sprintf("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s",$current_post_permalink);
+    $qr_code = sprintf('<img src="%s" alt="%s">',$image_src,$current_post_title);
+    $content .= $qr_code;
+    return $content;
 }
  add_filter( "the_content", "pqrc_display_qr_code");
 ?>
